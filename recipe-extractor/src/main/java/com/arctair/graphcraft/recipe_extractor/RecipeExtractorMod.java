3330e62d1759e46e5256ebbd12adf8b4e9990d54
tyler <tyler@cruftbusters.com>
2022-03-10 04:54:19 -0600

    example mod to recipe extractor mod

--- recipe-extractor/src/main/java/com/arctair/graphcraft/recipe_extractor/RecipeExtractorMod.java
+++ recipe-extractor/src/main/java/com/arctair/graphcraft/recipe_extractor/RecipeExtractorMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.arctair.graphcraft.recipe_extractor;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,25 +7,21 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+@Mod(modid = RecipeExtractorMod.MODID, name = RecipeExtractorMod.NAME, version = RecipeExtractorMod.VERSION)
+public class RecipeExtractorMod {
+    public static final String MODID = "recipe-extractor";
+    public static final String NAME = "JEI Recipe Extractor";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
+    public void init(FMLInitializationEvent event) {
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
