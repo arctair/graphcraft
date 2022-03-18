e16e1a66b0cda75443dbbe3bd9633e06dcd6b8ea
tyler <tyler@cruftbusters.com>
2022-03-17 21:10:22 -0500

    reset everything

--- api/test/test_up.py
+++ api/test/test_up.py
@@ -1,10 +1,3 @@
-def test_upgrade(client):
-    mc_path = '/home/arctair/.local/share/multimc/instances/nomifactory-dev-ab57eb2-client/minecraft'
-    with open(f'{mc_path}/recipes-v1.json', 'rb') as f:
-        response = client.put('/recipes-v1.json', data=f, headers={'Content-Type': 'application/json'})
-        assert response.status_code == 204
-
-    response = client.get('/recipes-v2.json')
-    assert response.status_code == 200
-    with open(f'{mc_path}/recipes-v2.json', 'wb') as f:
-        f.write(response.data)
+def test_up(client):
+    response = client.get('/')
+    assert response.status_code == 404
